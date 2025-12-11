6658173eea2ea39a73f9e57b814581e4458e4671
Deepanshu1230 <144600350+Deepanshu1230@users.noreply.github.com>
2025-12-11 20:57:34 +0530

    adding the Backend with the anothe import package

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import {BACKEND_URL} from "@repo/common/config";
+
+const app=express();
+
+
+console.log(BACKEND_URL);
+
+
+
+app.get("/",(req,res) =>{
+    res.json({
+        message:"heloo world"
+    })
+})
+
+
+app.listen(3000);
+
+
---
+++ packages/common/src/index.ts
@@ -0,0 +1,6 @@
+
+
+export const name="Deepanshu";
+
+export const BACKEND_URL="https://api.google.com";
+
